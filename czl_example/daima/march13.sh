52ccb054aa3fb0701b86fc822bdc559d7ea808e3
SIMONLQY <1269413527@qq.com>
2021-04-04 16:59:28 +0800

    从远程仓库删除不需要的文档

--- czl_example/daima/march13.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-python Main.py --dataset RV --trans rotat > danRVrotat.txt
-python Main.py --dataset FM --trans rotat > danFMrotat.txt
-python Main.py --dataset RV --trans rotat2 > danRVtotat2.txt
-python Main.py --dataset FM --trans rotat2 > danFMrotat2.txt
