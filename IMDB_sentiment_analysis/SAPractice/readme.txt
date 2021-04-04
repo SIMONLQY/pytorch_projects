b07b139295b13e7e4ed6528f7cae33a31b403709
SIMONLQY <1269413527@qq.com>
2021-04-04 17:01:43 +0800

    name change

---
+++ IMDB_sentiment_analysis/SAPractice/readme.txt
@@ -0,0 +1,7 @@
+Welcome to the Sentiment Analysis Practice Task!
+
+25000 txt files in /train are movie reviews with 'neg' and 'pos' in the file names demonstrating its sentiment polarity.
+25000 txt files in /test are movie reviews without annotation. Your task is to assign a positive or negative attitude to each and every of the 25000 test files.
+Your submission must be named 'answer.txt', and the answer.txt in this repository serves as a sample. Specifically, each line contains a file ID and a sentiment label, with 1 as positive and 0 as negative. The file IDs doesn't need to be ordered.
+
+For more information, please refer to the What's Next slides.
---
+++ IMDB_sentiment_analysis/my/old_file/my_dataset.py
@@ -0,0 +1,70 @@
+import os
+import re
+import argparse
+import torch
+import torch.nn as nn
+from torch.utils.data.dataset import Dataset
+from torch.utils.data import DataLoader
+import numpy as np
+import pandas as pd
+from my_model import MyLstm
+from torch import optim
+import torch.nn.functional as F
+from torch.nn.parallel import DistributedDataParallel as DDP
+
+def generate_tensor(sentence, sentence_max_size, embedding, word2id):
+    """
+    对一篇评论生成对应的词向量矩阵
+    :param sentence:一篇评论的分词列表
+    :param sentence_max_size:认为设定的一篇评论的最大分词数量
+    :param embedding:词向量对象
+    :param word2id:字典{word:id}
+    :return:一篇评论的词向量矩阵
+    """
+    tensor = torch.zeros([sentence_max_size, embedding.embedding_dim])
+    # UNK和PAD都取0
+    for index in range(0, sentence_max_size):
+        if index >= len(sentence):
+            break
+        else:
+            word = sentence[index]
+            if word in word2id:
+                vector = embedding.weight[word2id[word]]
+                tensor[index] = vector
+            elif word.lower() in word2id:
+                vector = embedding.weight[word2id[word.lower()]]
+                tensor[index] = vector
+    return tensor  # tensor是二维的，必须扩充为三维，否则会报错
+# 分词的API
+def tokenize(text):
+    # fileters = '!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n'
+    fileters = ['!', '"', '#', '$', '%', '&', '\(', '\)', '\*', '\+', ',', '-', '\.', '/', ':', ';', '<', '=', '>',
+                '\?', '@', '\[', '\\', '\]', '^', '_', '`', '\{', '\|', '\}', '~', '\t', '\n', '\x97', '\x96', '”',
+                '“', ]
+    text = re.sub("<.*?>", " ", text, flags=re.S)
+    text = re.sub("|".join(fileters), " ", text, flags=re.S)
+    return [i.strip() for i in text.split()]
+
+
+class MyDataset(Dataset):
+    def __init__(self, train_path, sentence_max_size, embedding, word2id, is_train=True):
+        ## record_path:记录数据路径及对应label的文件
+        self.is_train = is_train
+        self.embedding = embedding
+        self.word2id = word2id
+        self.sentence_max_size = sentence_max_size
+        self.train_path = train_path
+        self.data = os.listdir(train_path)
+
+    # 获取单条数据
+    def __getitem__(self, index):
+        text = open(self.train_path + self.data[index], encoding="utf-8").read().strip()
+        # text = np.loadtxt(self.train_path + self.data[index], dtype='str')
+        text = tokenize(text)
+        tensor = generate_tensor(text, self.sentence_max_size, self.embedding, self.word2id)
+        label = int(1 if self.data[index].split('_')[1].split('.')[0] == 'pos' else 0)
+        return tensor, label
+
+    # 数据集长度
+    def __len__(self):
+        return len(self.data)
