3ddde07e25e2db670fed836877720d45f4e5ce71
SIMONLQY <1269413527@qq.com>
2021-04-04 17:04:58 +0800

    删除文档

--- LSTM/my/answer_pro.py
+++
@@ -1,7 +0,0 @@
-with open('answer.txt','r') as f:
-    luan_data = f.read().strip().split('\n')
-    data = sorted(luan_data,key=(lambda x:int(x.split()[0])),reverse=True)
-with open('answer_2_lqy.txt','w') as f:
-    for i,d in enumerate(data):
-        f.write(d)
-        f.write('\n')
